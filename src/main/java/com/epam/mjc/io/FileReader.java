fc412f216126305471a90b6412611ead11ee6cb8
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-09-28 07:23:32 +0500

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -5,14 +5,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.logging.Logger;
 
 
 public class FileReader {
 
-    public  Profile getDataFromFile(File file) {
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(FileReader.class));
+
+    public Profile getDataFromFile(File file) {
+
 
         if (!file.exists()) {
-            System.out.println("File not found.");
+            LOGGER.log(LOGGER.getLevel(), "FILE NOT FOUND");
             return null;
         }
 
@@ -26,17 +30,17 @@ public class FileReader {
             String[] split = res.split("\n");
 
 
-            String name=split[0].substring(split[0].lastIndexOf(" ")).trim();
+            String name = split[0].substring(split[0].lastIndexOf(" ")).trim();
 
-            split[1]=split[1].substring(split[1].lastIndexOf(" ")).trim();
+            split[1] = split[1].substring(split[1].lastIndexOf(" ")).trim();
 
-            int age=Integer.parseInt(split[1]);
+            int age = Integer.parseInt(split[1]);
 
-            String email=split[2].substring(split[2].lastIndexOf(" ")).trim();
+            String email = split[2].substring(split[2].lastIndexOf(" ")).trim();
 
-            String longStr=split[3].substring(split[3].lastIndexOf(" ")).trim();
+            String longStr = split[3].substring(split[3].lastIndexOf(" ")).trim();
 
-            long phone=Long.parseLong(longStr);
+            long phone = Long.parseLong(longStr);
 
 
             return new Profile(name, age, email, phone);
